daf36653584ad1fe9b085def5e9bf310ffff93b8
wzh <421151849@qq.com>
2016-03-16 10:16:39 +0800

    搭建框架

---
+++ Wzh_UI_github/Wzh_UI_github/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wzh_UI_github
+//
+//  Created by 吴钟鸿 on 16/3/16.
+//  Copyright © 2016年 吴钟鸿. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
